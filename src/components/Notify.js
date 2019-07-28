e06faf157475cc8248f77c83a607ff197b9b40b1
BILLY ENDRES <billyendres@BILLYs-MacBook-Pro.local>
2019-07-28 16:23:57 +1000

    sent post request to server, still need to add styling

--- src/components/Notify.js
+++ src/components/Notify.js
@@ -4,30 +4,20 @@ import axios from "axios";
 const token = localStorage.getItem("Token");
 axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-axios
-	.post("http://35.201.2.209/notify", {
-		name: "Billy Endres",
-		email: "endres63@hotmail.com",
-		repoURL: "https://github.com/billyendres/frontend-dev-test",
-		message: ""
-	})
-	.then(res => {
-		res.status(200).send("Success");
-	})
-	.then(res => {
-		res.status(401).send("Access Denied");
-	})
-	.then(res => {
-		res.status(400).send("Bad Request");
-	});
+axios.post("http://35.201.2.209/notify", {
+	name: "Billy Endres",
+	email: "endres63@hotmail.com",
+	repoUrl: "https://github.com/billyendres/frontend-dev-test",
+	message: "How does a computer get drunk? It takes screenshots"
+});
 
-console.log(localStorage.getItem("Token"));
+console.log(token);
 
 class Notify extends Component {
 	render() {
 		return (
 			<div style={{ width: "100%", display: "flex", flexWrap: "wrap" }}>
-				Token: {localStorage.getItem("Token")}
+				Token: {token}
 			</div>
 		);
 	}
